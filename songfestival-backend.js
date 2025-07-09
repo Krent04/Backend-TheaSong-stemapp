84dac1e028a8ef9c1bda69f9b9e4529a100f1ebc
Krent04 <brent.schoemaker@icloud.com>
2025-07-09 20:36:57 +0200

    Update songfestival-backend.js

--- songfestival-backend.js
+++ songfestival-backend.js
@@ -71,7 +71,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    // 'https://theaterscholensongfestival-stem.netlify.app'
+    'https://theaterscholensongfestival-stemmen.netlify.app/'
   ],
   credentials: true,
 }));
